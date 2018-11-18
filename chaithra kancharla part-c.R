3a099e3b8c0816640e934f620ffdfbdcc6232cf5
geoffstrong55 <geoffstrong55@gmail.com>
2018-11-17 20:37:45 -0500

    Script contains edits to retrieval of data, the histogram, and the function from the vegan package.

--- chaithra kancharla part-c.R
+++ chaithra kancharla part-c.R
@@ -1,7 +1,8 @@
-###in part C module ihave choosen the biodiversity of mammalian taxonomic group. it has different orders and im interested in records of counntry and the number of orders in the data set, to reduce the selected data set and plot graph of reduced data.
-install.packages("tidyverse")
+##I have removed the install.packages("tidyverse"), install.packages("iNEXT") and install.packages("dplyr") from the original script. I have done this as it is not needed. Also, dplyr is a part of the tidyverse group of packages, so it does not need to be loaded separately. 
+
+###In part C module, I have choosen the biodiversity of mammalian taxonomic group. It has different orders and im interested in records of counntry and the number of orders in the data set, to reduce the selected data set and plot graph of reduced data.
 library(tidyverse)
-install.packages("dplyr")
+
 Assignmentdata <- read.delim("bold_data.txt")
 #to check records per country
 countries.count <-Assignmentdata %>%
@@ -12,7 +13,7 @@ countries.count <-Assignmentdata %>%
 #plot a histogram of the order_taxID
 hist(Assignmentdata$order_taxID)
 #to reduce the families
-top.families <- Assignment1data %>%
+top.families <- Assignmentdata %>% #Changed notation from Assignment1data to Assignmentdata as Assignment1data did not exist. 
   group_by(family_name) %>%
   mutate(count_by_family = length(processid)) %>%
   filter(count_by_family > 120 )
@@ -27,14 +28,14 @@ library(vegan)
 comm <- Assignmentdata %>%
   group_by(bin_uri) %>%
   count(bin_uri)
-commAssign1 <- specaccum(comm = n, method = "exact", permutations = 100,conditioned = TRUE)
-library(dplyr)
-commAssign1 <- spread(comm,bin_uri, n)
+commAssign1 <- specaccum(comm = n, method = "exact", permutations = 100, conditioned = TRUE)
+
+commAssign1 <- spread(comm, bin_uri, n)
 #using iNext package
-install.packages("iNext")
+
 library(iNEXT)
-g <-ggplot2::aes(top.families$family_name,top.families$count_by_family)
-g1<-ggplot2::aes(Assignmentdata$species_name,Assignmentdata$species_taxID)
+g <- ggplot2::aes(top.families$family_name, top.families$count_by_family)
+g1 <- ggplot2::aes(Assignmentdata$species_name, Assignmentdata$species_taxID)
 g1
 
 #####the data set is interpreted by checking the records of the country and  plotted the histogram to  analyze the qunatity of different orders. so with the function histogram mostly two orders are in high frequency.The data is reduced and filtered which has more 120 families. Vegan package and inext package are used to plot a graph.
